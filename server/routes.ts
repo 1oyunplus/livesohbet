5b25e16913b941469a9f54e4fde0e2273b2f8ad8
1oyunplus <1oyunplus@gmail.com>
2026-01-09 01:00:10 +0300

    Update storage import to use .js extension

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
+import { storage } from "./storage.js";
 import { WebSocketServer } from "ws";
 import type { WebSocket } from "ws";
 
