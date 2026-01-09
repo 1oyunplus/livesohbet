a3a215ac5b1873457ccbcdba114fe7446c68b5b8
1oyunplus <1oyunplus@gmail.com>
2026-01-09 11:57:27 +0300

    Update import statement for storage module

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
 import { type Server } from "http";
-import { storage } from "./storage";
+import { storage } from "./storage.js";
 import { WebSocketServer } from "ws";
 import type { WebSocket } from "ws";
 
