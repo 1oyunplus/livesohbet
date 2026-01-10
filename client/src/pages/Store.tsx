322f6021f7050ddcaafdb4abf1b093d5191b7e42
KullaniciAdin <mailadresin@example.com>
2026-01-10 19:17:05 +0300

    Güncelleme

--- client/src/pages/Store.tsx
+++ client/src/pages/Store.tsx
@@ -9,11 +9,11 @@ export default function Store() {
 
   const diamondPackages = [
     { amount: 100, price: 25, bonus: 0 },
-	{ amount: 225, price: 50, bonus: 25 },standart: true },
+	{ amount: 225, price: 50, bonus: 25 },
     { amount: 350, price: 75, bonus: 50 },
 	{ amount: 475, price: 100, bonus: 75 },
 	{ amount: 600, price: 125, bonus: 100 },popular: true },
-    { amount: 1200, price: 250, bonus: 200, popular: true },
+    { amount: 1200, price: 250, bonus: 200,
     { amount: 2400, price: 500, bonus: 400 },
   ];
 
